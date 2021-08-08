b14da1250d5d72063aa6b25c2d28a1a5be22127b
Nguyen Minh Tri <trinmse140981@fpt.edu.vn>
2021-08-08 19:07:22 +0700

    heroku

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import postRoutes from './routers/posts.js'
+require('dotenv').config()
 
 
 const app = express();
@@ -14,7 +15,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://trinm:Pucapro1032@cluster0.yqpdz.mongodb.net/second_project?retryWrites=true&w=majority'
+const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.yqpdz.mongodb.net/second_project?retryWrites=true&w=majority`
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
